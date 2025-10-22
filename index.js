41e1374ecfb844f8a088e6f81d534ef9a7e87bd1
Luis Gutierrez <lagaguate@yahoo.com>
2025-10-22 10:27:04 -0600

    server web

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Guatemala!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
